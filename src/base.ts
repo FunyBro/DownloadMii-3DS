aca31cf33da1845caf2a5a8c0b9bb0ea23bae515
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-10-30 14:25:34 +0100

    Add AllApps view.

--- src/base.ts
+++ src/base.ts
@@ -1,5 +1,16 @@
+//    _____                      _                 _ __  __ _ _ 
+//   |  __ \                    | |               | |  \/  (_|_)
+//   | |  | | _____      ___ __ | | ___   __ _  __| | \  / |_ _ 
+//   | |  | |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` | |\/| | | |
+//   | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| | |  | | | |
+//   |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|_|  |_|_|_|
+//                                                              
+//   ©2014-2015 filfat Studios AB
+//   Licensed under the MIT license
+
 /// <reference path="DownloadMii.ts" />
 /// <reference path="views.ts" />
+
 declare var Network: any;
 declare var System: any;
 declare var Input: any;
--- src/views.ts
+++ src/views.ts
@@ -1,54 +1,76 @@
+//    _____                      _                 _ __  __ _ _ 
+//   |  __ \                    | |               | |  \/  (_|_)
+//   | |  | | _____      ___ __ | | ___   __ _  __| | \  / |_ _ 
+//   | |  | |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` | |\/| | | |
+//   | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| | |  | | | |
+//   |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|_|  |_|_|_|
+//                                                              
+//   ©2014-2015 filfat Studios AB
+//   Licensed under the MIT license
+
 module views {
-	export function Main(runtime: DownloadMii.Base) {
-		var position = 0;
-		var items = [
-			{
-				title: 'View Apps',
-				render: function(runtime: DownloadMii.Base) {
-					
-				},
-			},
-			{
-				title: 'Settings',
-				render: function(runtime: DownloadMii.Base) {
-					
-				},
-			},
-			{
-				title: 'Exit',
-				render: function(runtime: DownloadMii.Base) {
-					System.exit();
-				},
-			},
-		];
+	export function AllApps(runtime: DownloadMii.Base) {
+		var pos = 0,
+			page = 0,
+			pages = 0;
+			
+		var items = [];
 		
-		var Rendered = false;
-		while(1){
-			var input = Input.getInput();
-			if(!input.KEY && Rendered)
-				continue;
-			else if(input.KEY_A) {
-				items[position].render(runtime);
-			} else if(input.KEY_DOWN) {
-				position = (position + 1) <= (items.length - 1) ? (position + 1) : 0;
-			} else if(input.KEY_UP) {
-				position = (position - 1) >= 0 ? (position - 1) : (items.length - 1);
+		//Temporary data
+		for(var x = 0; x < 100; x++){
+			var entry = new DownloadMii.App();
+			entry.version = '1.0';
+			entry.name = 'App (' + x + ')';
+			items.push(entry);
+		}
+			
+		pages = Math.ceil(items.length/21);	
+		while(1) {
+			var input = Input.getInputDown();
+			if(input.KEY_B)
+				break;
+			else if(input.KEY_A)
+				App(runtime, items[pos+(21*page)]);
+			else if(input.KEY_RIGHT){
+				if((page+1) >= pages)
+					page = 0;
+				else
+					++page;
+			} else if(input.KEY_LEFT){
+				if(page > 0)
+					--page;
+				else
+					page = pages-1;
+			} else if(input.KEY_UP){
+				//TODO
+				--pos;
+			} else if(input.KEY_DOWN){
+				//TODO
+				++pos;
 			}
 			
 			Console['clear']();
-			Console['print']("\n\n  DownloadMii vXXXX\n\n\n\n");
-			for(var x = 0; x < items.length; x++){
-				var item = items[x];
-				Console['print']((position == x ? " >" : "  ") + item.title + "\n");
+			Console['print']("\n\n  All Apps\n\n");
+			for(var x = 0; x < 21; x++) {
+				var r = (x+(21*page));
+				if(r > (items.length-1)){
+					Console['print']("\n");
+					continue;
+				}
+				var item: DownloadMii.App = items[r];
+				if(pos === x)
+					Console['print'](" >" + item.name + "\n");
+				else
+					Console['print']("  " + item.name + "\n");
 			}
+			
+			Console['print']("\n  < Page " + (page+1) + " of " + pages + " >");
 			Console['flushBuffers']();
-			Rendered = true;
 		}
 	}
-	
 	export function App(runtime: DownloadMii.Base, app: DownloadMii.App) {
 		while(1) {
-			var input = Input.getInputUp();
+			var input = Input.getInputDown();
 			if(input.KEY_B)
 				break;
 			else if(input.KEY_A) {
@@ -65,6 +87,49 @@ module views {
 			Console['print']("\n\n  " + app.name + " v" + app.version + "\n\n\n\n");
 			Console['print']("  A: Download\n");
 			Console['print']("  B: Go back\n");
+			Console['flushBuffers']();
+		}
+	}
+	
+	export function Main(runtime: DownloadMii.Base) {
+		var position = 0;
+		var items = [
+			{
+				title: 'All Apps',
+				render: function(runtime: DownloadMii.Base) {
+					AllApps(runtime);
+				},
+			},
+			{
+				title: 'Settings',
+				render: function(runtime: DownloadMii.Base) {
+					
+				},
+			},
+			{
+				title: 'Exit',
+				render: function(runtime: DownloadMii.Base) {
+					System.exit();
+				},
+			},
+		];
+		
+		while(1){
+			var input = Input.getInputDown();
+			if(input.KEY_A) {
+				items[position].render(runtime);
+			} else if(input.KEY_DOWN) {
+				position = (position + 1) <= (items.length - 1) ? (position + 1) : 0;
+			} else if(input.KEY_UP) {
+				position = (position - 1) >= 0 ? (position - 1) : (items.length - 1);
+			}
+			
+			Console['clear']();
+			Console['print']("\n\n  DownloadMii vXXXX\n\n\n\n");
+			for(var x = 0; x < items.length; x++){
+				var item = items[x];
+				Console['print']((position == x ? " >" : "  ") + item.title + "\n");
+			}
 			Console['flushBuffers']();
 		}
 	}
