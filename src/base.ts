d76552b45d4b27025a30280ab47aa6aeb0a869d6
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-11-02 08:53:57 +0100

    Update base.ts

--- src/base.ts
+++ src/base.ts
@@ -5,7 +5,7 @@
 //   | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| | |  | | | |
 //   |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|_|  |_|_|_|
 //                                                              
-//   ©2014-2015 filfat Studios AB
+//   © 2014-2015 filfat Studios AB
 //   Licensed under the MIT license
 
 /// <reference path="DownloadMii.ts" />
